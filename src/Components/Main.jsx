73cd766294d1100d183be2ca797e9ed5dc2195c2
walid-amjahdi <w.amjahdi3996@uca.ac.ma>
2026-07-31 13:04:53 +0100

    2:15 31/7

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -2,13 +2,7 @@ export function Main()
 {
     return (
         <>
-        <h2>a title for the list :</h2>
-        <ul>
-            <li>first</li>
-            <li>second</li>
-            <li>third</li>
-            <li>fourth</li>
-        </ul>
+            <p>today's stop : 2:15</p>
         </>
     )    
 }
