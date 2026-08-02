d2083f5dc4592f4a43eed44c7f993934a8311983
walid-amjahdi <w.amjahdi3996@uca.ac.ma>
2026-08-02 12:02:55 +0100

    4:00 2/8

---
+++ src/Data/test.js
@@ -0,0 +1,14 @@
+// const nums = [1,2,3,4,5]
+// const doubleNums = nums.map((num)=>(num * num))
+// console.log(nums)
+// console.log(doubleNums)
+
+// const names = ["name1" ,"name2" ,"name3"]
+// const upperNames = names.map((str)=>(
+//     str.charAt(0).toUpperCase() + str.slice(1)
+// ))
+
+// console.log(upperNames)
+
+const pokemons = ["Bulbasaur","Charmander","Squirtle"]
+const modifiedPokemons = pokemons.map((pokemon)=>(<p>{pokemon}</p>))
