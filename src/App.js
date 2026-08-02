291dc0edaf21b539ba5b00647110130f10584806
walid-amjahdi <w.amjahdi3996@uca.ac.ma>
2026-08-02 12:21:11 +0100

    4:00 2/8 more modifications

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
 import './Styles/App.css';
 import { Header } from './Components/Header';
 import { Entry } from './Components/Entry';
+import Data from './Data/Data.js'
+
 
 function App() {
+
+  const entry = Data.map((data) => (
+    <Entry 
+        img={data.img}
+        title={data.title}
+        country={data.country}
+        dates={data.dates}
+        text={data.text} 
+        />
+  ))
+
   return (
     <>
       <Header />
-      <Entry 
-        img={{
-          src: "https://scrimba.com/links/travel-journal-japan-image-url",
-          alt: "Mount Fuji"
-        }}
-        title="Mount Fuji"
-        country="📍Japan"
-        dates="12 Jan, 2021 - 24 Jan, 2021"
-        text="Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists."
-      />
-      </>
+      {entry}
+    </>
   );
 }
 
